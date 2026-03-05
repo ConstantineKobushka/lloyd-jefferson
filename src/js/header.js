bbd624a597cc86ffd12e808f6915f74eb7f4ef27
Constantine Kobushka <kobushkaconstantine@gmail.com>
2026-03-05 15:51:38 +0200

    change a css selector's name

--- src/js/header.js
+++ src/js/header.js
@@ -10,15 +10,15 @@ function onThemeControlsClick(event) {
   if (!event.target.matches('button')) return;
 
   Array.from(event.currentTarget.children).forEach(function (item) {
-    item.classList.remove('active');
+    item.classList.remove('current-color');
   });
 
-  event.target.classList.add('active');
+  event.target.classList.add('current-color');
 
   const currentColor = event.target.dataset.color;
   body.id = currentColor;
 }
 
 function onMenuBtnClick() {
-  menu.classList.toggle('active');
+  menu.classList.toggle('show-menu');
 }
