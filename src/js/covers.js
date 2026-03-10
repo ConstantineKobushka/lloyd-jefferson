ceed52efe8e7b171862ef0ac357c523b310c97ff
Constantine Kobushka <kobushkaconstantine@gmail.com>
2026-03-10 16:24:52 +0200

    add covers section

---
+++ src/js/covers.js
@@ -0,0 +1,18 @@
+const coversSection = document.querySelector('.covers-section');
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px 0px 0px 0px',
+  threshold: 0.5,
+};
+
+const observerCallBack = entries => {
+  if (entries[0].isIntersecting) {
+    coversSection.classList.add('animate');
+  } else {
+    coversSection.classList.remove('animate');
+  }
+};
+
+const observer = new IntersectionObserver(observerCallBack, observerOptions);
+observer.observe(coversSection);
